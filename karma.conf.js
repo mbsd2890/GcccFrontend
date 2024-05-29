35d26d9bf65e2ce193715d9a579475817178cb4e
mbsd2890 <mbsd2890@scs.ubbcluj.ro>
2024-05-29 15:35:10 +0300

    modified name

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/WSMTfe'),
+      dir: require('path').join(__dirname, './coverage/fe'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'WSMTfe'`, () => {
+  it(`should have as title 'fe'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('WSMTfe');
+    expect(app.title).toEqual('fe');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('WSMTfe app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('fe app is running!');
   });
 });
