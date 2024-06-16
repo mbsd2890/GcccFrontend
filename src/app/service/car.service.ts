f553747d7e0e127c7d8a5d015bb46bff2cd21dfa
mbsd2890 <mbsd2890@scs.ubbcluj.ro>
2024-06-16 14:22:26 +0300

    changed api

--- src/app/service/car.service.ts
+++ src/app/service/car.service.ts
@@ -2,43 +2,44 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-const BASIC_URL = 'https://wonderful-bay-04b3f7303.5.azurestaticapps.net';
-
 @Injectable({
   providedIn: 'root',
 })
 export class CarService {
+  //private baseUrl = 'http://localhost:8080/cars/';
+  private baseUrl = 'https://gcccbackend.azurewebsites.net/cars/';
+
   constructor(private http: HttpClient) {}
 
   postCar(car: any): Observable<any> {
     return this.http.post(
-      'https://gcccbackend.azurewebsites.net/api/car',
+      `${this.baseUrl}`,
       car
     );
   }
 
   getAllCars(): Observable<any> {
     return this.http.get(
-      'https://gcccbackend.azurewebsites.net/api/cars'
+      `${this.baseUrl}`
     );
   }
 
   getCarById(id: number): Observable<any> {
     return this.http.get(
-      'https://gcccbackend.azurewebsites.net/api/car/' + id
+      `${this.baseUrl}` + id
     );
   }
 
   updateCar(id: number, car: any): Observable<any> {
     return this.http.put(
-      'https://gcccbackend.azurewebsites.net/api/car/' + id,
+      `${this.baseUrl}` + id,
       car
     );
   }
 
   deleteCar(id: number): Observable<any> {
     return this.http.delete(
-      'https://wonderful-bay-04b3f7303.5.azurestaticapps.net/api/car/' + id
+      `${this.baseUrl}` + id
     );
   }
 }
